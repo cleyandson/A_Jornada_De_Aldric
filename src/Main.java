8016c66ac7d11b7d6554a15394fe8246429d6220
Cleyandson Gustavo <cleyandson@hotmail.com>
2024-05-10 16:21:14 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import edu.cleyandson.personagem.arsenal_do_protagonista.Armas;
+import edu.cleyandson.personagem.inimigos.Inimigo;
+import edu.cleyandson.personagem.protagonista.Aldric;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Introdução ao Mundo
+        System.out.println("Bem-vindo a Eterídea, um mundo de fantasia repleto de perigos e aventuras!");
+        System.out.println("Você é Aldrich, um ex-cavaleiro nobre caído em desgraça após uma traição cruel");
+        System.out.println("Agora, você busca redenção em sua jornada épica para limpar seu nome e enfrentar o mal do Rei Demônio que assombra esse mundo!");
+        System.out.println("Prepare-se para uma jornada cheia de desasfios!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Criação do protagonista e sua arma inicial
+        Armas espadaFalcaoNegro = new Armas("Espada - Falcão da Aurora Negra", 10);
+        Aldric aldrich = new Aldric("Aldrich", 1, espadaFalcaoNegro);
+
+        //Criação do primeiro inimigo
+        Inimigo caveleiroSombrio = new Inimigo("Caveleiro Sombrio do Rei Demônio", 50, 5, 5);
+
+        //Introdução ao encontro com o primeiro inimigo
+        System.out.println("Você avança pela Floresta Sombria, quando de repente se depara com um Caveleiro do Rei Demônio!");
+
+        //Dialógo com o servo o primeiro inimigo
+        System.out.println( caveleiroSombrio.getNome() + ": Você ousa desafiar os servos do Rei Demônio? Prepare-se para enfrentar sua ruína!");
+
+        //Início da batalha entre Aldrich e o Caveleiro Sombrio do Rei Demônio
+        Aldric aldric;
+        System.out.println("\nBatalha começando! " + aldrich.getNome() + " contra " + caveleiroSombrio.getNome());
+
+
+
     }
 }
