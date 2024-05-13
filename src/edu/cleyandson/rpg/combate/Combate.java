4b7f572af0a085c7e3660e637631a2478c5f38c5
Cleyandson Gustavo <cleyandson@hotmail.com>
2024-05-13 15:20:03 -0300

    commit

--- src/edu/cleyandson/rpg/combate/Combate.java
+++ src/edu/cleyandson/rpg/combate/Combate.java
@@ -5,5 +5,7 @@ import edu.cleyandson.rpg.personagem.Personagem;
 public interface Combate {
     void atacar(Personagem alvo);
     void poderEspecial(Personagem alvo);
+    void incrementarEnergia(int valor);
+    void ultimate(Personagem alvo);
 }
 
--- src/edu/cleyandson/rpg/protagonista/Aldric.java
+++ src/edu/cleyandson/rpg/protagonista/Aldric.java
@@ -2,6 +2,7 @@ package edu.cleyandson.rpg.protagonista;
 
 import edu.cleyandson.rpg.arsenal_do_protagonista.Armas;
 import edu.cleyandson.rpg.combate.Combate;
+import edu.cleyandson.rpg.inimigos.Inimigo;
 import edu.cleyandson.rpg.personagem.Personagem;
 
 public class Aldric extends Personagem implements Combate {
@@ -10,6 +11,7 @@ public class Aldric extends Personagem implements Combate {
     private int exp;
     private boolean desonrado;
     private Armas arma;
+    private int energia;
 
     //Construtores
     public Aldric(Armas arma){
@@ -18,6 +20,7 @@ public class Aldric extends Personagem implements Combate {
         this.exp = 0;
         this.desonrado = false;
         this.arma = arma;
+        this.energia = 0;
     }
 
     //Getters & Setters
@@ -86,7 +89,15 @@ public class Aldric extends Personagem implements Combate {
 
     }
 
-    //Método para chegar o status atual do personagem
+    public int getEnergia(){
+        return this.energia;
+    }
+
+    public void setEnergia(int energia){
+        this.energia = energia;
+    }
+
+    // Método para chegar o status atual do personagem
     public void status(){
         System.out.println("Nome: " + getNome());
         System.out.println("Nível" + getNivel());
@@ -98,35 +109,106 @@ public class Aldric extends Personagem implements Combate {
         }
     }
 
-    //Método atacar os inimigos
+    // Método para incrementar energia
+    @Override
+    public void incrementarEnergia(int valor) {
+        this.energia += valor;
+    }
+
+    // Método para atacar
     @Override
     public void atacar(Personagem alvo) {
-        //Verficar se está com alguma arma na mão
+        // Incrementar 7 de energia a cada ataque
+        incrementarEnergia(7);
+
         int danoBase = getAtaque();
+
+        // Verficar se está com alguma arma na mão
         if(arma != null){
             danoBase += arma.getQuantidadePoder();
             System.out.println(getNome() + " empunhou sua " + getArma() + " e agora está com " + getAtaque() + " de ataque!");
         }
 
-        //Calcular o dano considerando a defesa do inimigo
+        // Calcular o dano considerando a defesa do inimigo
         int danoTotal  = Math.max(0, danoBase - alvo.getDefesa());
 
-        //Reduzindo a vida do inimigo com base no dano
+        // Reduzindo a vida do inimigo com base no dano
         alvo.setVida(alvo.getVida() - danoTotal);
 
-        //Calcular o dano real que o inimigo recebeu e printar pro usuário
+        // Calcular o dano real que o inimigo recebeu e printar pro usuário
         int danoReal = alvo.getVida() - (alvo.getVida() - danoTotal);
         System.out.println(getNome() + " atacou " + alvo.getNome() + " causando " + danoReal + " de dano!");
 
     }
 
-    //Método para soltar o poder especial quando atingir x de energia
+    // Método para soltar o poder especial quando atingir x de energia
     @Override
     public void poderEspecial(Personagem alvo) {
-        //implementar lógica do poder
+        // Incrementa 15 de energia a cada especial
+        incrementarEnergia(15);
+
+        // Verifica se o alvo é um inimigo
+        if(alvo instanceof Inimigo){
+
+            // Verficar se está com alguma arma na mão
+            int danoBase = getAtaque();
+
+            if(arma != null){
+                danoBase += arma.getQuantidadePoder();
+            }
+
+            // O poder especial multiplica o dano base
+            int danoDuplicado = danoBase * 2;
+
+            // Calcula o dano total considerando a defesa do alvo
+            int danoDoEspecial = Math.max(0, danoDuplicado - alvo.getDefesa());
+
+            // Reduz a vida do alvo com base no dano
+            alvo.setVida(alvo.getVida() - danoDoEspecial);
+
+            // Printa pro usuário o especial e o dano
+            System.out.println(getNome() + " usou seu poder especial e causou" + danoDoEspecial);
+        } else {
+            System.out.println("O poder de " + getNome() + " só pode ser usado contra a força do Rei Demônio.");
+        }
     }
 
-    //Método que realiza o UP de nível do personagem para o próx level com os novos status
+    @Override
+    public void ultimate(Personagem alvo) {
+        // Verifica se tem energia  o suficiente para usar a ultimate
+        if(this.energia < 100){
+            System.out.println(getNome() + " não tem energia o suficiente para usar seu Ultimate");
+            return;
+        }
+
+        // Verifica se o alvo é um inimigo
+        if (alvo instanceof Inimigo){
+            int danoBase  =  getAtaque();
+
+            if(arma != null){
+                danoBase += arma.getQuantidadePoder();
+            }
+
+            // O Ultimate triplica o dano base
+            int danoDaUltimate = danoBase * 3;
+
+            // Lógica do dano real da ultimate
+            int danoReal = Math.max(0, danoDaUltimate - alvo.getDefesa());
+
+            // Setando o dano real no inimigo
+            alvo.setVida(alvo.getVida() - danoReal);
+
+            System.out.println(getNome() + " usou seu Ultimate  e  causou " + danoReal + " de dano!");
+
+            // Reseta a energia após a Ultimate
+            setEnergia(0);
+
+        } else {
+            System.out.println("O Ultimate de " +getNome() + " só pode ser usado contra a força do Rei Demônio");
+        }
+    }
+
+    //   Método que realiza o UP de nível do personagem para o próx level com os novos status
     public void upar(){
         this.nivel++;
         this.exp = 0;
